25bf99dd492324ac29a45469a24d4877019baac0
Katia Villavicencio <125634854+KatiaVillavicencio@users.noreply.github.com>
2023-10-17 17:43:49 -0500

    95%

--- src/dao/models/carts.model.js
+++ src/dao/models/carts.model.js
@@ -4,9 +4,22 @@ const cartCollection = 'carts';
 
 const cartSchema = new mongoose.Schema({
 
-    description: {type: String, required: true},
-    quantity:{type: Number, required:true},
-    total: { type: Number, required: true}
+    products: {
+        type:[
+            {
+                _id:{
+                    type: mongoose.Types.ObjectId,
+                    ref: 'products'
+                },
+                quantity:{
+                    type: Number,
+                    default:1
+                }
+                    
+            }
+        ],
+        default:[]
+    }
 });
 cartSchema.pre('find', function(next){
     this.populate('products._id');
